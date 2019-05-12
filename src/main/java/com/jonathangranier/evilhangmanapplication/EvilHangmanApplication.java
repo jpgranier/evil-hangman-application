84e8481bbf1806d03602e04b83788c4f0f3ac499
Jonathan Granier <jgranier99@gmail.com>
2019-05-11 20:07:05 -0500

    Created Spring Boot project outline

---
+++ src/main/java/com/jonathangranier/evilhangmanapplication/EvilHangmanApplication.java
@@ -0,0 +1,13 @@
+package com.jonathangranier.evilhangmanapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvilHangmanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EvilHangmanApplication.class, args);
+    }
+
+}
